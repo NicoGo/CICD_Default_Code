26f3fb223898a7c10e484824711bb565924e8c2a
Nicolas Brandstett <nicolasbrandstett@MacBook-Pro-de-Nicolas.local>
2024-09-19 15:40:07 +0800

    short change

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-   return 'Here on the website'
+   return 'Here on the website with change'
 
 if __name__ == '__main__':
    app.run(host ='0.0.0.0', port=802, debug = True)
