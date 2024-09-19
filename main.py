d0cc03d8639159fc08db6278258d56d5b31bc475
Nicolas Brandstett <nicolasbrandstett@MacBook-Pro-de-Nicolas.local>
2024-09-19 11:40:52 +0800

    new code including flask

--- main.py
+++ main.py
@@ -1,25 +1,9 @@
-'''
-A simple command line tool that takes 2 values and adds them together using
-the calc.py library's 'add2' function.
-'''
+from flask import Flask, render_template
+app = Flask(__name__)
 
-import sys
-import functions
+@app.route('/')
+def hello():
+   return 'Here on the website'
 
-argnumbers = len(sys.argv) - 1
-
-if argnumbers == 2 :
-    print("")
-    print("The result is " + str(functions.add2(str(sys.argv[1]), str(sys.argv[2]))))
-    print("")
-    sys.exit(0)
-
-if argnumbers != 2 :
-    print("")
-    print("You entered " + str(argnumbers) + " value/s.")
-    print("")
-    print("Usage: 'add2vals X Y' where X and Y are individual values.")
-    print("       If add2vals is not in your path, usage is './add2vals X Y'.")
-    print("       If unbundled, usage is 'python add2vals.py X Y'.")
-    print("")
-    sys.exit(1)
+if __name__ == '__main__':
+   app.run(host ='0.0.0.0', port=802, debug = True)
